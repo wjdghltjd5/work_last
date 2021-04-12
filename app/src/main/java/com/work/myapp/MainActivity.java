1412eb60de6c679fccbd1153ce6c81ded7cd4e83
wjdghltjd5 <79885750+wjdghltjd5@users.noreply.github.com>
2021-04-13 01:24:19 +0900

    Initial commit

---
+++ app/src/main/java/com/work/myapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.work.myapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton main1, main2, main3, main4;
+    View.OnClickListener cl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(Color.BLACK);
+        }
+        setContentView(R.layout.activity_main);
+
+        main1 = (ImageButton) findViewById(R.id.mainicon_1);
+        main2 = (ImageButton) findViewById(R.id.mainicon_2);
+        main3 = (ImageButton) findViewById(R.id.mainicon_3);
+        main4 = (ImageButton) findViewById(R.id.mainicon_4);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch ( v.getId() ) {
+                    case R.id.mainicon_1: {
+                        Intent intent = new Intent(MainActivity.this, Map_Acivity.class);
+                        startActivity(intent);
+                    }
+                    break;
+                    case R.id.mainicon_2: {
+                        Toast.makeText(getApplicationContext(), "미구현 기능입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                    case R.id.mainicon_3: {
+                        Toast.makeText(getApplicationContext(), "미구현 기능입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                    case R.id.mainicon_4: {
+                        Toast.makeText(getApplicationContext(), "미구현 기능입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                }
+            }
+        };
+        main1.setOnClickListener(cl);
+        main2.setOnClickListener(cl);
+        main3.setOnClickListener(cl);
+        main4.setOnClickListener(cl);
+    }
+}
---
+++ app/src/main/java/com/work/myapp/Map_Acivity.java
@@ -0,0 +1,103 @@
+package com.work.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class Map_Acivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.POIItemEventListener {
+
+    ImageButton home;
+    MapView mapView;
+    RelativeLayout mapViewContainer;
+    View.OnClickListener cl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(Color.BLACK);
+        }
+        setContentView(R.layout.map_layout);
+
+        home = (ImageButton) findViewById(R.id.home_button);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        home.setOnClickListener(cl);
+
+        mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        mapView = new MapView(this);
+        mapView.setPOIItemEventListener(this);
+        mapViewContainer.addView(mapView);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+        MapPoint MARKER_POINT = net.daum.mf.map.api.MapPoint.mapPointWithGeoCoord(37.4488, 127.1678);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(marker);
+
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+
+        MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
+        MapPoint currentMapPoint = MapPoint.mapPointWithGeoCoord(mapPointGeo.latitude, mapPointGeo.longitude);
+        mapView.setMapCenterPoint(currentMapPoint, true);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+}
